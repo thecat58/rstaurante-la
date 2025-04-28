8c3174c7f75d0be07761e2f03cf3e0ba64c89671
chavito07 <kb3223739115@gmail.com>
2025-04-28 18:02:43 -0500

    nuevos modelos bsd

--- menu_restaurante_backend/app/models.py
+++ menu_restaurante_backend/app/models.py
@@ -1,15 +1,5 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-# filepath: c:\Users\KIKE\Desktop\rstaurante-la\menu_restaurante_backend\Role, Permission, User, UserRole, RolePermission, UserPapp\models.py
 from .modeloP import *
-
-
 from django.db import models
 
 
